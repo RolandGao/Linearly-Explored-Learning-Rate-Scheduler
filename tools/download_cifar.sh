71244ce21d854bc6b7ffbab2f510df4d1a618112
Richard Huang <42769475+Xiaorui-Huang@users.noreply.github.com>
2022-04-12 15:49:01 -0400

    data download bug fix

--- tools/download_cifar.sh
+++ tools/download_cifar.sh
@@ -1,6 +1,6 @@
 mkdir pycls/datasets/data
 cd pycls/datasets/data
-wget curl https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz
 mv cifar-10-batches-py cifar10
